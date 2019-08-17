250f7224c9d88f95a50272f7d924bf4132fa5e26
Yih Yang <yyngo788n88@hotmail.com>
2019-08-17 15:18:48 +0800

    generated admin/home #index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get '/', to: 'home#index'
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
