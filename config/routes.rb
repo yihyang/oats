13bb7eea26c7504466df41ba1720516272e2c56b
Yih Yang <yyngo788n88@hotmail.com>
2019-08-18 14:05:53 +0800

    added jobs API

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,15 @@ Rails.application.routes.draw do
     resources :locations
   end
   devise_for :users
+
+  namespace :api do
+    namespace :v1 do
+      resources :jobs, only: [:index] do
+        collection do
+          get '/group-by-locations-and-departments', to: 'jobs#group_by_locations_and_departments'
+        end
+      end
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
