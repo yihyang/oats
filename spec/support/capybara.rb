1d0250f4767318a7b84d41af00d7b642a76c1904
Yih Yang <yyngo788n88@hotmail.com>
2019-08-25 17:04:32 +0800

    added ':nocov:' to rspec files

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,6 +4,7 @@ require 'selenium/webdriver'
 
 # By default Capybara will use Selenium+Firefox for `js:true` feature specs.
 # The following lines will tell Capybara to use Chrome instead of Firefox,
+# :nocov:
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
